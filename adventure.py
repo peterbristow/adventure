44185c71947af46946632878a48bb880a2f82f69
Peter <peterjb73@gmail.com>
2016-04-01 13:48:44 +0100

    fixes #2: add print statement

--- adventure.py
+++ adventure.py
@@ -25,4 +25,5 @@ while True:
     if valid_directions.get(direction):
         position = valid_directions[direction]
     else:
+        print "sorry, that isn't a val;id direction"
         continue
